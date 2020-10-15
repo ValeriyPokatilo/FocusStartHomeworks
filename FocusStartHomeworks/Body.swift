5f102802cfb3a5b8123db73f18cfa30a5a33841e
Valeriy <Valeriy>
2020-10-16 05:09:22 +0700

    Создание моделей Car и Body

---
+++ FocusStartHomeworks/Body.swift
@@ -0,0 +1,16 @@
+//
+//  Body.swift
+//  FocusStartHomeworks
+//
+//  Created by Valeriy Pokatilo on 16.10.2020.
+//
+
+import Foundation
+
+enum Body {
+	case Unknow
+	case Sedan
+	case SUV
+	case Coupe
+	case Сabriolet
+}
