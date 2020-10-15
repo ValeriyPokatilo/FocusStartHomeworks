99068cb85e5b579b8353e8212bb4560f136b224b
Valeriy <Valeriy>
2020-10-16 05:12:00 +0700

    Функционал добавления автомобиля

---
+++ FocusStartHomeworks/BodySelector.swift
@@ -0,0 +1,35 @@
+//
+//  BodySelector.swift
+//  FocusStartHomeworks
+//
+//  Created by Valeriy Pokatilo on 16.10.2020.
+//
+
+import Foundation
+
+class BodySelector {
+
+	static let shared = BodySelector()
+
+	func enterBodyType() -> Body {
+
+		var bodyNumber: String!
+		var body = Body.Unknow
+
+		while body == Body.Unknow {
+
+			MessageManager.shared.bodyTypeList()
+			bodyNumber = readLine()
+
+			switch bodyNumber {
+			case "1": body = Body.Sedan
+			case "2": body = Body.SUV
+			case "3": body = Body.Coupe
+			case "4": body = Body.Сabriolet
+			default: body = Body.Unknow
+			}
+		}
+
+		return body
+	}
+}
---
+++ FocusStartHomeworks/CarCreator.swift
@@ -0,0 +1,63 @@
+//
+//  CarCreator.swift
+//  FocusStartHomeworks
+//
+//  Created by Valeriy Pokatilo on 16.10.2020.
+//
+
+import Foundation
+
+class CarCreator {
+
+	static let shared = CarCreator()
+
+	func addCar() -> Car {
+
+		MessageManager.shared.separator()
+		print("Добавление нового автомобиля")
+
+		let car = Car(manufacturer: enterStringData(field: "производителя"),
+					  model: enterStringData(field: "модель"),
+					  body: BodySelector.shared.enterBodyType(),
+					  yearOfIssue: enterIntData(field: "год выпуска"),
+					  carNumber: enterOptStringData(field: "гос номер"))
+
+		return car
+	}
+
+	func enterStringData(field: String) -> String {
+
+		var value = ""
+
+		while value.isEmpty {
+			print("Введите \(field) автомобиля")
+			value = readLine() ?? ""
+		}
+
+		return value
+	}
+
+	func enterOptStringData(field: String) -> String? {
+
+		print("Введите \(field) автомобиля")
+		let carNumber = readLine()
+
+		if carNumber == "" {
+			return nil
+		} else {
+			return carNumber
+		}
+	}
+
+	func enterIntData(field: String) -> Int? {
+
+		print("Введите \(field) автомобиля")
+		let yearOfIssue = readLine()
+
+		if let year = Int(yearOfIssue ?? ""), year != 0 {
+			return year
+		} else {
+			return nil
+		}
+	}
+}
