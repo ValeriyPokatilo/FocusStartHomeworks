12495aba5bbedaba77a1be31e18b1b31646df88d
Valeriy <Valeriy>
2020-10-23 02:01:01 +0700

    Добавлен второй проет и изменена структура папок проекта

---
+++ HomeWork-2/HomeWork-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-2
+//
+//  Created by Valeriy Pokatilo on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
