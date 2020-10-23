1b1e1a6b4cc85b7a050127f040e352aef161b393
Valeriy <Valeriy>
2020-10-23 16:06:11 +0700

    Реализована проверка массива

--- HomeWork-2/HomeWork-2/ViewController.swift
+++ HomeWork-2/HomeWork-2/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var threadSafeArray = ThreadSafeArray<Int>()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		demonstration()
 	}
 
+	func demonstration() {
+		let queue = DispatchQueue(label: "", attributes: .concurrent)
 
+		queue.async(flags: .barrier) {
+			for number in 0...1000 {
+				self.threadSafeArray.append(number)
+				print("Итерация №\(number), элементов в потокобезопасном массиве \(self.threadSafeArray.count)")
+			}
+
+			print("Итого: \(self.threadSafeArray.count) элементов в массиве")
+		}
+	}
 }
 
