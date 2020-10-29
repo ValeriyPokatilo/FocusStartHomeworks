8b7235431a383c0f84e914a5414154983845d2c5
Valeriy <Valeriy>
2020-10-29 22:54:04 +0700

    Создан проект для ДЗ 3

---
+++ HomeWork-3/HomeWork-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-3
+//
+//  Created by Valeriy Pokatilo on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
