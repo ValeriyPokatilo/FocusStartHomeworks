ab164efbbc0fc2c9942b6040fb437ce764191213
Valeriy <Valeriy>
2020-11-15 13:23:00 +0700

    Создан проект, определена структура файлов проекта

---
+++ HomeWork-5/HomeWork-5/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  HomeWork-5
+//
+//  Created by Valeriy Pokatilo on 15.11.2020.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+		window?.windowScene = windowScene
+		window?.rootViewController = CurrencyViewController()
+		window?.makeKeyAndVisible()
+	}
+}
+
---
+++ HomeWork-5/HomeWork-5/CurrencyList/CurrencyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyViewController.swift
+//  HomeWork-5
+//
+//  Created by Valeriy Pokatilo on 15.11.2020.
+//
+
+import UIKit
+
+class CurrencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		self.view.backgroundColor = .green
+    }
+}
