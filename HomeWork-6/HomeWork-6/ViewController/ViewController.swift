5232c115f409d9ea07765de5c27fdaf2c2e22fb9
Valeriy <Valeriy>
2020-11-22 02:02:27 +0700

    Создан проект №6, определена структура папок проекта

---
+++ HomeWork-6/HomeWork-6/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-6
+//
+//  Created by Valeriy Pokatilo on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .red
+	}
+
+
+}
+
