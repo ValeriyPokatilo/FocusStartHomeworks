1fe56ab09728b052e5b4a610e349afb039e25fcb
Valeriy <Valeriy>
2020-12-03 23:04:43 +0700

    Создание метрик и цветовых схем

---
+++ HomeWork-7/HomeWork-7/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  HomeWork-7
+//
+//  Created by Valeriy Pokatilo on 03.12.2020.
+//
+
+import UIKit
+
+struct Colors {
+	static let backgroundColor = UIColor.systemGreen
+	static let getButtonColor = UIColor.systemYellow
+}
---
+++ HomeWork-7/HomeWork-7/Metrics.swift
@@ -0,0 +1,24 @@
+//
+//  Metrics.swift
+//  HomeWork-7
+//
+//  Created by Valeriy Pokatilo on 03.12.2020.
+//
+
+import UIKit
+
+struct Metrics {
+	static let tableViewToUpSeparator: CGFloat = 70
+	static let tableViewHeight: CGFloat = 140
+	static let imageSize: CGFloat = 90
+	static let minimumSizeSeparator: CGFloat = 2
+	static let smallSizeSeparator: CGFloat = 6
+	static let textHeight: CGFloat = 20
+	static let standartSizeSeparator: CGFloat = 16
+	static let labelTopSeparator: CGFloat = 82
+	static let stackViewSpacing: CGFloat = 12
+	static let verticalSeparator: CGFloat = 12
+	static let mediumSizeSeparator: CGFloat = 22
+	static let bigSizeSeparator: CGFloat = 32
+	static let buttonWidth: CGFloat = 35
+}
