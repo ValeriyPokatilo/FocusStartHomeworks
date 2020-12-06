7b77c15648c57db4cbefa4e3522c268a73b2ad75
Valeriy <Valeriy>
2020-12-06 16:02:09 +0700

    Добавление нового проекта и определение его структуры

---
+++ HomeWork-8/HomeWork-8/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  HomeWork-8
+//
+//  Created by Valeriy Pokatilo on 06.12.2020.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+		window?.windowScene = windowScene
+		let mainViewController = ViewController()
+		let navigationMainViewController = UINavigationController(rootViewController: mainViewController)
+		window?.rootViewController = navigationMainViewController
+		window?.makeKeyAndVisible()
+	}
+}
+
---
+++ HomeWork-8/HomeWork-8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-8
+//
+//  Created by Valeriy Pokatilo on 06.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .orange
+	}
+
+
+}
+
